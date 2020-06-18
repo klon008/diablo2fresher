6be86928a33a257ce5747c4dc3c138fcc785dd21
klon_008 <klon_008@mail.ru>
2020-06-18 16:29:49 +0700

    fix path to dist url

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  	chainWebpack: config => {
+    config.module
+      .rule('images')
+        .use('url-loader')
+          .loader('url-loader')
+      },
 	publicPath: process.env.NODE_ENV === 'production'
 	? '/diablo2fresher/'
 	: '/'
